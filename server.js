3bfdb8b21786f804e6ef84f1c03fb1d418e05791
Han Khuu <h.khuu01@gmail.com>
2017-05-19 16:16:02 -0400

    updated server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-// Requiring our Note and Article models
-var note = require("./models/note.js");
-var article = require("./models/article.js");
+
 // Our scraping tools
 var request = require("request");
-var cheerio = require("cheerio"); 
+var cheerio = require("cheerio");
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
+
 // Set Handlebars
 var exphbs = require("express-handlebars");
 var Handlebars = require("handlebars");
